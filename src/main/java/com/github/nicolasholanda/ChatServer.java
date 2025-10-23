9afbdc0ce69f8546e5d7156dd70e013792a95f18
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-10-23 09:51:33 -0300

    Started defining proto

---
+++ src/main/java/com/github/nicolasholanda/ChatServer.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.List;
+
+public class ChatServer {
+
+}
